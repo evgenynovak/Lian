20ca590cdb6f499543db930461c66f39bae6a4c9
evgenynovak <evnovak171@mail.ru>
2019-03-28 13:59:40 +0300

    added slider

---
+++ src/js/test.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  dots: true,
+  margin:1,
+  nav:true,
+  navText : ["",""],
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
