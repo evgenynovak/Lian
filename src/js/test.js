785592f2c7b0838a0bb08868063f606fc91bc10f
evgenynovak <evnovak171@mail.ru>
2019-05-01 22:27:06 +0300

    test.js: fix added function

--- src/js/test.js
+++ src/js/test.js
@@ -1,34 +1,36 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  dots: true,
-  margin:1,
-  nav:true,
-  navText : ["",""],
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
-})
+$( document ).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    dots: true,
+    margin:1,
+    nav:true,
+    navText : ["",""],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
 
-$('.portfolio__list').masonry({
-  itemSelector: '.portfolio__item',
-  columnWidth: 360,
-  gutter: 30,
-  isResizable: true,
-  isAnimated: true
-});
+  $('.portfolio__list').masonry({
+    itemSelector: '.portfolio__item',
+    columnWidth: 360,
+    gutter: 30,
+    isResizable: true,
+    isAnimated: true
+  });
 
-$('.blog__posts').masonry({
-  itemSelector: '.post',
-  columnWidth: 360,
-  gutter: 30,
-  isResizable: true,
-  isAnimated: true
+  $('.blog__posts').masonry({
+    itemSelector: '.post',
+    columnWidth: 360,
+    gutter: 30,
+    isResizable: true,
+    isAnimated: true
+  });
 });
